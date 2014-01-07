5cf59e41cca4fdb441f1bb55ef9957e5ff5deaa1
sconno05 <sconnolly17@gmail.com>
2014-01-06 21:09:54 -0700

    Initial creation of Web API solution

---
+++ Phonetics.Tests/Controllers/ValuesControllerTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Phonetics;
+using Phonetics.Controllers;
+
+namespace Phonetics.Tests.Controllers
+{
+    [TestClass]
+    public class ValuesControllerTest
+    {
+        [TestMethod]
+        public void Get()
+        {
+            // Arrange
+            PhoneticsController controller = new PhoneticsController();
+
+            // Act
+            IEnumerable<string> result = controller.Get(1);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(2, result.Count());
+            Assert.AreEqual("dog", result.ElementAt(0));
+            Assert.AreEqual("cat", result.ElementAt(1));
+        }
+    }
+}
---
+++ Phonetics/Controllers/PhoneticsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Phonetics.Controllers
+{
+    public class PhoneticsController : ApiController
+    {
+        public IEnumerable<string> Get(int number)
+        {
+            var suggestions = new List<string>();
+            suggestions.Add("dog");
+            suggestions.Add("cat");
+            return suggestions;
+        }
+    }
+}
